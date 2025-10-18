db87be7b6723e8bcfeef743815ada79eef334467
iahmedkoc <i.ahmedkoc@gmail.com>
2025-10-18 22:23:11 +0300

    ok

---
+++ src/test/java/US401_LoginErrorValidation/ismailAhmed.java
@@ -0,0 +1,7 @@
+package US401_LoginErrorValidation;
+
+public class ismailAhmed {
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
---
+++ src/test/java/US402_UserLoginFunctionality/ismailAhmed.java
@@ -0,0 +1,7 @@
+package US402_UserLoginFunctionality;
+
+public class ismailAhmed {
+    public static void main(String[] args) {
+        System.out.println("s");
+    }
+}
