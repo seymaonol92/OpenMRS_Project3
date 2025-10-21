6faca3065cab31eade9b3111f55e81ed98ca51a4
Gul Seyma Onol <g.cibisoglu92@gmail.com>
2025-10-22 02:02:25 +0300

    pages created

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -4,46 +4,59 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+import java.util.List;
+
 public class LoginPage extends BasePage{
 
-    @FindBy(xpath = "//div[@class='login-form']/h2")
-    private WebElement accountText;
+    @FindBy(xpath = "//legend[@class='w-auto']")
+    private WebElement loginHeaderText;
 
-    @FindBy(xpath = "//input[@data-qa='login-email']")
-    private WebElement emailInput;
+    @FindBy(id = "username")
+    private WebElement usernameInput;
 
-    @FindBy(xpath = "//input[@data-qa='login-password']")
+    @FindBy(id = "password")
     private WebElement passwordInput;
 
-    @FindBy(xpath = "//button[@data-qa='login-button']")
+    @FindBy(id = "loginButton")
     private WebElement loginBtn;
 
-    // cssSelector -> input[data-qa='login-password']+p
-    @FindBy(xpath = "//input[@data-qa='login-password']/following-sibling::p")
-    private WebElement errorMessage;
+    @FindBy(xpath = "//li[@id='Inpatient Ward']")
+    private WebElement inpatientWardLocation;
+
+    @FindBy(id = "error-message")
+    private WebElement invalidMessage;
 
     public LoginPage(final WebDriver driver) {
         super(driver);
     }
 
     public void checkPage() {
-        verifyDisplayed(accountText, "Login sayfasi acilmadi");
-        LOGGER.info("Verify login page opened");
+        verifyDisplayed(usernameInput, "The login page did not open (username field not found).");
+        verifyDisplayed(passwordInput, "he login page did not open (password field not found).");
+        verifyDisplayed(loginBtn,      "The login page did not open (login button not found).");
+        LOGGER.info("Login page verified");
     }
 
-    public void fillLoginMask(final String email, final String password) {
-        sendKeysToElement(emailInput, email);
-        LOGGER.info("Email entered");
+    public void fillLoginForm(final String username, final String password) {
+        sendKeysToElement(usernameInput, username);
+        LOGGER.info("Username entered");
         sendKeysToElement(passwordInput, password);
         LOGGER.info("Password entered");
     }
+
+    public void selectInpatientWard() {
+        verifyDisplayed(inpatientWardLocation, "The ‘Inpatient Ward’ location is not visible.");
+        clickElement(inpatientWardLocation);
+        LOGGER.info("'Inpatient Ward' location selected");
+    }
+
     public void clickLoginBtn(){
         clickElement(loginBtn);
         LOGGER.info("Login button clicked");
     }
-    public void checkErrorMessage() {
-        verifyDisplayed(errorMessage, "Hata mesaji görünmüyor");
-        LOGGER.info("Verify error message displayed");
+    public void checkInvalidCredentialsMessage() {
+        verifyDisplayed(invalidMessage, "The invalid username/password warning is not visible.");
+        LOGGER.info("Invalid credentials message visible");
     }
 
 }
