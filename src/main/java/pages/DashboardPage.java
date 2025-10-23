cbc44a1745dfc7d9d2121b7349031982ed716bf1
Gul Seyma Onol <g.cibisoglu92@gmail.com>
2025-10-23 19:49:09 +0300

    dashboardPage created

--- src/main/java/pages/DashboardPage.java
+++ src/main/java/pages/DashboardPage.java
@@ -18,8 +18,46 @@ public class DashboardPage extends BasePage {
     @FindBy(xpath = "//*[@id='search-input-:rer:']")
     private WebElement sendKeySearchBox;
 
+    @FindBy(xpath = " //*[@id=\"single-spa-application:@openmrs/esm-primary-navigation-app-page-0\"]/div/header/div[3]/div[1]/div[3]/span/div/button/svg")
+    private WebElement clickAddPatient;
+
+    @FindBy(xpath = "//*[name()='svg' and .//*[name()='use' and contains(@href,'omrs-icon-user-avatar')]]")
+    private WebElement myAccountLink ;
+
+    @FindBy(xpath = "//*[name()='use' and contains(@href,'omrs-icon-switcher')]/parent::*")
+    private WebElement appMenu ;
+
+    @FindBy(xpath = "//a[contains(text(),'System Administration')]")
+    private WebElement systemAdministration ;
+
+    @FindBy(xpath = "//div[@data-extension-id='service-queues-dashboard-link']")
+    private WebElement serviceQueues;
+
+    @FindBy(xpath = "//div[@data-extension-id='clinical-appointments-dashboard-link']")
+    private WebElement appointments;
+
+    @FindBy(xpath = "//div[@data-extension-id='patient-lists-dashboard-link']")
+    private WebElement patientLists;
+
+    @FindBy(xpath = "//div[@data-extension-id='ward-dashboard-link']")
+    private WebElement wards;
+
+    @FindBy(xpath = "//div[@data-extension-id='laboratory-dashboard-link']")
+    private WebElement laboratory;
+
+
     public DashboardPage(final WebDriver driver) {
         super(driver);
+    }
+
+    public void verifyOpenMRSText() {
+        verifyDisplayed(openMRSText, "OpenMRS text is not displayed");
+        LOGGER.info("OpenMRS text verified");
+    }
+
+    public void verifyLocationText() {
+        verifyDisplayed(locationText, "Location text is not displayed");
+        LOGGER.info("Location text verified");
     }
 
     public void clickSearchBox(){
@@ -30,4 +68,45 @@ public class DashboardPage extends BasePage {
         sendKeysToElement(sendKeySearchBox, patient);
         LOGGER.info("Patient name entered");
     }
+    public void navbarClickAddPatient(){
+        clickElement(clickAddPatient);
+        LOGGER.info("Add patient clicked");
+    }
+    public void clickMyAccountLink(){
+        clickElement(myAccountLink);
+        LOGGER.info("My accoount clicked");
+    }
+    public void verifyAppMenu() {
+        verifyDisplayed(appMenu, "App Menu is not displayed");
+        LOGGER.info("App Menu verified");
+    }
+    public void clickAppMenu(){
+        clickElement(appMenu);
+        LOGGER.info("App Menu clicked");
+    }
+    public void clickSystemAdministration(){
+        clickElement(systemAdministration);
+        LOGGER.info("System Administration clicked");
+    }
+    public void clickServiceQueues(){
+        clickElement(serviceQueues);
+        LOGGER.info("Service Queues clicked");
+    }
+    public void clickAppointments(){
+        clickElement(appointments);
+        LOGGER.info("Appointments clicked");
+    }
+    public void clickPatientLists(){
+        clickElement(patientLists);
+        LOGGER.info("Patient lists clicked");
+    }
+    public void clickWards(){
+        clickElement(wards);
+        LOGGER.info("Wards clicked");
+    }
+    public void clickLaboratory(){
+        clickElement(laboratory);
+        LOGGER.info("Laboratory clicked");
+    }
+
 }
